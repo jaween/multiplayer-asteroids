ab2cd4d51f8868c4a078ae2b9a02d4051b0a8c22
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2019-06-27 13:23:17 +0930

    Clean up dirs and build script

--- server/build.sh
+++ server/build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-rm -r build
-mkdir build
-dart2js -O0 -o build/out.js dart/main.dart
+mkdir -p build
+
+if [ "$1" = "--release" ];
+then
+  dart2js -O2 -o build/out.js dart/main.dart
+elif [ "$1" = "" ]
+then
+  dart2js -O0 -o build/out.js dart/main.dart
+else
+  echo "Invalid parameter $1"
+  exit 1
+fi
 cat js/dart_main_runner.js js/node_preamble.js build/out.js > build/main.js
-rm build/out.*
